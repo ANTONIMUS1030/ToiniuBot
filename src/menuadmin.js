429a9fc9af50a9167d61e8d0cdd50349765faf8b
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-02-13 00:14:20 -0400

    help.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -1,33 +1,113 @@
-const menuadmin = (prefix, pushname) => {
- return `oiin aqui e o menu dos admins
- _obs para o bot executar esses comandos e nescessario dar adm para o bot_
- 
- ◪ *COMANDO DOS ADMINS*
- │
- ├─ ❏ ${prefix}opengc
- ├─ ❏ ${prefix}closegc
- ├─ ❏ ${prefix}promote
- ├─ ❏ ${prefix}demote
- ├─ ❏ ${prefix}tagall
- ├─ ❏ ${prefix}tagall2
- ├─ ❏ ${prefix}tagall3
- ├─ ❏ ${prefix}tagall4
- ├─ ❏ ${prefix}tagall5
- ├─ ❏ ${prefix}add
- ├─ ❏ ${prefix}kick
- ├─ ❏ ${prefix}listadmins
- ├─ ❏ ${prefix}linkgroup
- ├─ ❏ ${prefix}leave
- ├─ ❏ ${prefix}welcome
- ├─ ❏ ${prefix}nsfw
- ├─ ❏ ${prefix}leveling
- ├─ ❏ ${prefix}level
- ├─ ❏ ${prefix}delete
- ├─ ❏ ${prefix}simih
- └─ ❏ ${prefix}ownergroup
- `
-
-
+const help = (prefix) => {
+	return `
+╔═══════════════════
+║                           
+║     𝕊𝔼ℝ𝔾𝕀ℕ 𝔻𝕆 𝔾ℝ𝔸𝕍𝔼
+║
+╠═══════════════════
+║ 	    • ──── ✾ ──── •
+╠═══ » Sticker Comandos «
+║       • ──── ✾ ──── •
+  ➸ comando : *${prefix}sticker* ou *${prefix}stiker*
+  ➸ desc : converter imagem / gif / vídeo em adesivo
+  ➸ uso : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda\n
+  ➸ comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+  ➸ desc : converte imagem em sticker enquanto remove o fundo
+  ➸ uso : responder imagem ou enviar imagem com legenda\n
+  ➸ comando : *${prefix}toimg*
+  ➸ desc : converter sticker em imagem
+  ➸ uso : adesivo de resposta\n
+  ➸ comando : *${prefix}tsticker* ou *${prefix}tstiker*
+  ➸ desc : converter texto em sticker
+  ➸ uso : *${prefix}tsticker text in here*\n
+        
+          • ──── ✾ ──── •
+         » Comandos Meme «
+   	      • ──── ✾ ──── •
+  ➸ comando : *${prefix}meme*
+  ➸ desc : imagens aleatórias de meme [inglês]
+  ➸ uso : basta enviar o comando\n
+  ➸ comando : *${prefix}memeindo*
+  ➸ desc : imagens aleatórias de meme [indo]
+  ➸ uso : apenas envie o comando\n
+          • ──── ✾ ──── •
+         » Outros Comandos «
+          • ──── ✾ ──── •
+  ➸ comando : *${prefix}dono*
+  ➸ desc : mostra as informações do dono do bot
+  ➸ uso : apenas envie o comando\n
+  ➸ comando : *${prefix}gtts*
+  ➸ desc : converter texto em fala/áudio
+  ➸ uso : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
+  ➸ comando : *${prefix}loli*
+  ➸ desc : imagens aleatórias de loli
+  ➸ uso : apenas envie o comando\n
+  ➸ comando : *${prefix}loli1*
+  ➸ desc : imagens aleatórias de loli
+  ➸ uso : apenas envie o comando\n
+  ➸ comando : *${prefix}nsfwloli*
+  ➸ desc : imagens aleatórias de nsfw loli
+  ➸ uso : apenas envie o comando\n
+  ➸ comando : *${prefix}url2img*
+  ➸ desc : tirar screenshots da web
+  ➸ uso : *${prefix}url2img [tipe] [url]*\n
+  ➸ comando : *${prefix}simi*
+  ➸ desc : sua mensagem será respondida por simi
+  ➸ uso : *${prefix}simi sua mensagem*\n
+  ➸ comando : *${prefix}ocr*
+  ➸ desc : pegue o texto na foto
+  ➸ uso : responder imagem ou enviar imagem com legenda\n
+  ➸ comando : *${prefix}wait*
+  ➸ desc : pesquisar anime com imagem [ Que anime é este/Que ]
+  ➸ uso : responder imagem ou enviar imagem com legenda\n
+  ➸ comando : *${prefix}setprefix*
+  ➸ desc : substituir prefixo
+  ➸ uso : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
+  ➸ Nota : Este comando só pode ser usado pelo proprietário do bot\n
+          • ──── ✾ ──── •
+     » Comandos para Grupos «
+          • ──── ✾ ──── •
+  ➸ comando : *${prefix}play*
+  ➸ desc : se tiver pegando é para baixar musica
+  ➸ uso : *${prefix}play nome da musica*\n
+  ➸ comando : *${prefix}add*
+  ➸ desc : adicionar membro ao grupo
+  ➸ uso : *${prefix}add 55929xxxxx*\n
+  ➸ Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+  ➸ comando : *${prefix}kick*
+  ➸ desc : chutar membros do grupo
+  ➸ usag : *${prefix}kick @da pessoa*\n
+  ➸ Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+  ➸ comando : *${prefix}promote*
+  ➸ desc : tornar o membro do grupo como administrador do grupo
+  ➸ uso : *${prefix}promote @da pessoa*\n
+  ➸ Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+  ➸ comando : *${prefix}demote*
+  ➸ desc : tornar o administrador do grupo como membro do grupo
+  ➸ uso : *${prefix}demote @da pessoa*\n
+  ➸ Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+  ➸ comando : *${prefix}linkgroup*
+  ➸ desc : pegue o link do grupo
+  ➸ uso : just send the command
+  ➸ Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+  ➸ comando : *${prefix}leave*
+  ➸ desc : Faça o bot sair do grupo
+  ➸ uso : apenas envie o comando
+  ➸ Nota : Só pode ser usado por administradores de grupo e proprietário do bot\n
+  ➸ comando : *${prefix}tagall*
+  ➸ desc : marca todos os membros do grupo, incluindo administradores também
+  ➸ uso : apenas envie o comando
+  ➸ Nota : Este comando pode ser usado se você for um administrador de grupo\n
+  ➸ comando : *${prefix}simih*
+  ➸ desc : ative o modo simi no grupo
+  ➸ uso : *${prefix}simih 1* para ativar o modo simi e *${prefix}simih 0* para desativar o modo simi
+  ➸ Nota : Este comando pode ser usado se você for um administrador de grupo\n`
+  ╔═══════════════════
+  ║        FEITO POR
+  ║
+  ║       ANTONIMUS†
+  ╚═══════════════════
+  
 }
 
-exports.menuadmin = menuadmin
+exports.help = help
